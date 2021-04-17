e9ce55041e4eaf7932c7fbfd5716b75fec7312b2
Himanshu Sharma <him.sharma8686@gmail.com>
2021-04-17 20:13:24 +0530

    Pass safe args from choose receiver fragment to send cash fragment

--- app/src/main/java/com/androidxjetpacknavigation/ChooseReceiverFragment.kt
+++ app/src/main/java/com/androidxjetpacknavigation/ChooseReceiverFragment.kt
@@ -1,5 +1,28 @@
 package com.androidxjetpacknavigation
 
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_choose_receiver.*
 
-class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver)
+class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver){
+
+    private lateinit var receiverName : String
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       receiverName = et_receiver_name.text.toString()
+
+        btn_next.setOnClickListener {
+             this.receiverName = et_receiver_name.text.toString()
+
+            val action = ChooseReceiverFragmentDirections
+                .actionChooseReceiverFragmentToSendCashFragment(receiverName=receiverName,amount = 400)
+
+            findNavController().navigate(action)
+
+        }
+    }
+}
